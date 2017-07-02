58d92cb4d10a4d9b89e112ba88208d702e7e8c83
Sanjeev Sachdev <sanjeevsachdev@gmail.com>
2017-07-02 22:06:42 +0530

    Adding a REST-based hello world appdone with Spring Boot.

---
+++ hello-world-rest-interface-with-spring-boot/src/main/java/io/sanjeevsachdev/Application.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package io.sanjeevsachdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Sanjeev Sachdev
+ *
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ hello-world-rest-interface-with-spring-boot/src/main/java/io/sanjeevsachdev/GreetingController.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package io.sanjeevsachdev;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Sanjeev Sachdev
+ * 
+ */
+@RestController
+public class GreetingController {
+	private static final String template = "Hello, %s!";
+
+	@RequestMapping(method = RequestMethod.GET, value = "/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {		
+		return new Greeting(String.format(template, name));
+	}
+}
